f981f3843137ee3393a7a198f9091115f8957496
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-24 20:53:57 -0300

    Obtém repositório e armazena propriedade full_name do mesmo.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa';
 import {Container, Form, SubmitButton} from './styles';
 
+import api from '../../services/api';
+
 export default function Main(){
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorios, setRepositorios] = useState([]);
+
+
+  const handleSubmit = useCallback((e)=>{
+    e.preventDefault();
+
+    async function submit(){ 
+      const response = await api.get(`repos/${newRepo}`);
+  
+      const data = {
+        name: response.data.full_name,
+      }
+  
+      setRepositorios([...repositorios, data]);
+      console.log(repositorios);
+      setNewRepo('');
+    }
+
+    submit();
+
+  }, [newRepo, repositorios]);
+
+  function handleinputChange(e){
+    setNewRepo(e.target.value);
+  }
+
   return(
     <Container>      
       <h1>
@@ -10,8 +39,14 @@ export default function Main(){
         Meus Repositorios
       </h1>
 
-      <Form onSubmit={()=>{}}>
-        <input type="text" placeholder="Adicionar Repositorios"/>
+      <Form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Adicionar Repositorios"
+          value={newRepo}
+          onChange={handleinputChange}
+        />
+
         <SubmitButton>
           <FaPlus color="#FFF" size={14}/>
         </SubmitButton>
