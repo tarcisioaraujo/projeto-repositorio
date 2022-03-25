c8c277264eb28a29b4af7f57c6590df8a1cba2dc
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-24 21:28:27 -0300

    Lista repositórios adicionados com estilização e função de excluir.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,11 @@
 import React, {useState, useCallback} from 'react';
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
-import {Container, Form, SubmitButton} from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 
 import api from '../../services/api';
 
 export default function Main() {
+
   const [newRepo, setNewRepo] = useState('');
   const [repositorios, setRepositorios] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -36,6 +37,12 @@ export default function Main() {
     setNewRepo(e.target.value);
   }
 
+  const handleDelete = useCallback((repo)=> {
+    const find = repositorios.filter(r => r.name !== repo);
+    setRepositorios(find);
+  }, [repositorios]);
+
+
   return(
     <Container>      
       <h1>
@@ -59,6 +66,22 @@ export default function Main() {
           )}
         </SubmitButton>
       </Form>
+
+      <List>
+         {repositorios.map(repo => (
+           <li key={repo.name}>
+             <span>
+             <DeleteButton onClick={()=> handleDelete(repo.name) }>
+                <FaTrash size={14}/>
+             </DeleteButton>  
+             {repo.name}
+             </span>
+             <a href="">
+               <FaBars size={20}/>
+             </a>
+           </li>
+         ))} 
+      </List>
 
     </Container>
   )
